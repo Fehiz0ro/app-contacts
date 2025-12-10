27fec26b6e7e943f42e57cb2aaa62f31b4c4e9f2
Fehiz0ro <zzorox9@gmail.com>
2025-12-10 16:31:37 +0300

    rectification fichier .sh

--- buildscript.sh
+++ buildscript.sh
@@ -14,8 +14,7 @@ docker-compose up -d
 echo "Attendre 10 secondes..."
 sleep 10
 
-
-MYIP=$(ip -4 addr show enp0s3 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')
+MYIP=$(hostname -I | awk '{print $1}')
 
 echo "Test du backend..."
 curl http://$MYIP:5000/contacts
